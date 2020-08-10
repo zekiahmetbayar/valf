663a6fb86ad4a6274f71a6e8464fa1aa0193b11a
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2020-08-10 14:58:12 +0300

    Tablo yapısı güncellendi. Notebook komponenti ile çift tab eklendi.

--- main.py
+++ main.py
@@ -12,16 +12,32 @@ class MyWindow(Gtk.Window):
 
     def list_view(self):
         table = Gtk.Table(n_rows=3, n_columns=3, homogeneous=True)
-        self.add(table)
         listbox = Gtk.ListBox()
+        self.add(table)
         self.add(listbox)
-        array = ["Bu","Bir","Array","Merhaba"]
 
+        array = ["Bu","Bir","Array","Merhaba"]
         for i in array:
             items = Gtk.Label(i)
             listbox.add(items)
 
         table.attach(listbox,0,1,0,3)
+
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
+
+        self.page1 = Gtk.Box()
+        self.page1.set_border_width(10)
+        self.page1.add(Gtk.Label(label="Default Page!"))
+        self.notebook.append_page(self.page1, Gtk.Label(label="Page1"))
+        
+
+        self.page2 = Gtk.Box()
+        self.page2.set_border_width(10)
+        self.page2.add(Gtk.Label(label="A page with an image for a Title."))
+        self.notebook.append_page(self.page2, Gtk.Label(label="Page2"))
+
+        table.attach(self.notebook,1,3,0,3)
  
 
  
