7756ac118d26d03b9f59613ed12929859dfa3b00
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2020-08-11 21:55:29 +0300

    Artık 'New Page' Notebook'a yeni sayfa ekliyor.

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from gi.repository import Gdk
-
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class MyWindow(Gtk.Window):
+    notebook = Gtk.Notebook()
+
     def __init__(self):
         Gtk.Window.__init__(self)
         self.set_default_size(750, 500)
         self.connect("destroy", Gtk.main_quit)
         self.list_view()
         
-
     def list_view(self):
-        table = Gtk.Table(n_rows=3, n_columns=3, homogeneous=True)
+        self.table = Gtk.Table(n_rows=3, n_columns=3, homogeneous=True)
         listbox = Gtk.ListBox()
-        self.add(table)
+        self.add(self.table)
         self.add(listbox)
 
         self.two_d_array = {'Hello' : 'Hi', 'Example' : 'Merhaba'}
@@ -24,34 +24,24 @@ class MyWindow(Gtk.Window):
             items = Gtk.Button.new_with_label(i)
             items.connect("button-press-event",self.button_clicked)
             listbox.add(items)
-
-        table.attach(listbox,0,1,0,3)
-
-        self.notebook = Gtk.Notebook()
+        self.table.attach(listbox,0,1,0,3)
+        
         self.add(self.notebook)
+        self.table.attach(self.notebook,1,3,0,3)
 
+        self.notebook.show_all()
         self.page1 = Gtk.Box()
         self.page1.set_border_width(10)
-        self.page1.add(Gtk.Label(label="Default Page!"))
-        self.notebook.append_page(self.page1, Gtk.Label(label="Page1"))
-        
-
-        self.page2 = Gtk.Box()
-        self.page2.set_border_width(10)
-        self.page2.add(Gtk.Label(label="A page with an image for a Title."))
-        self.notebook.append_page(self.page2, Gtk.Label(label="Page2"))
-
-        table.attach(self.notebook,1,3,0,3)
-    
+        self.page1.add(Gtk.Label(label="Merhaba bu ilk sayfa."))
+        self.notebook.append_page(self.page1, Gtk.Label(label="Default Page"))
 
     def context_menu(self):
         menu = Gtk.Menu()
-        menu_item = Gtk.MenuItem("New")
+        menu_item = Gtk.MenuItem("New Page")
         menu.append(menu_item)
         menu_item.connect("activate", self.on_click_popup)
         menu.show_all()
 
-        
         return menu
 
     ##  Buton sağ click ise context menu açtı
@@ -68,15 +58,13 @@ class MyWindow(Gtk.Window):
         if event.button == 3:
             widget.popup(None, None, None, None, event.button, event.time)
         
-    def on_click_popup(self, action):
-        about_dialog = Gtk.Window()
-        about_dialog.set_title("New Window")
-        screen_label = Gtk.Label(label = self.two_d_array[self.labelmenu])
-        about_dialog.add(screen_label)
-        about_dialog.present()
-        about_dialog.show_all()
- 
-
+    def on_click_popup(self, action):   
+        ## Yeni sayfa oluştur     
+        self.new_page = Gtk.Box()
+        self.new_page.set_border_width(10)
+        self.new_page.add(Gtk.Label(label=self.two_d_array[self.labelmenu]))
+        self.notebook.append_page(self.new_page, Gtk.Label(label="New Page"))
+        self.notebook.show_all()
         
         
 window = MyWindow()
